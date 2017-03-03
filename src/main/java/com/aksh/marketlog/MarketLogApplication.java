66d75b5ec749fbf468760d4197f2c82e87c7899a
Akshaya Rawat <akshaya.rawat@gmail.com>
2017-03-02 23:51:04 -0800

    Heroku

--- src/main/java/com/aksh/marketlog/MarketLogApplication.java
+++ src/main/java/com/aksh/marketlog/MarketLogApplication.java
@@ -1,5 +1,8 @@
 package com.aksh.marketlog;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -43,7 +47,7 @@ public class MarketLogApplication {
 	private String username;
 	@Value("${db.datasource.password:junk}")
 	private String passwd;
-	@Bean 
+
 	public DataSource dataSource(){
 		BasicDataSource basicDataSource =new BasicDataSource();
 		
@@ -55,6 +59,29 @@ public class MarketLogApplication {
 		
 		return basicDataSource;
 	}
+	
+	
+	@Bean
+//	@Profile("heroku")
+	public DataSource dataSourceHeroku() throws URISyntaxException{
+		BasicDataSource basicDataSource =new BasicDataSource();
+
+		URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+	    String username1 = dbUri.getUserInfo().split(":")[0];
+	    String password1 = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+		basicDataSource.setDriverClassName(driverClass);
+		basicDataSource.setUrl(dbUrl);
+		basicDataSource.setUsername(username1);
+		basicDataSource.setPassword(password1);
+		logger.info("Datasource - "+driverClass+":"+url+":"+username1+":"+password1);
+		
+		return basicDataSource;
+	}
+	
+	
 	
 	@Bean
 	public DataSourceInitializer dataSourceInitializer(DataSource dataSource){
